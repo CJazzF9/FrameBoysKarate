6fd1f0a8e47f087aec0ddc8b238bdda468aff7b8
CJazzF9 <CJazzF@gmail.com>
2025-03-28 20:25:45 -0600

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Frame Brothers Karate</h1>
+      <p>Representing Team USA on the road to the World Championship in Germany.</p>
+    </main>
+  );
+}
