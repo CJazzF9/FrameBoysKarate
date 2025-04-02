e0fbbd055568dc9d34e08a9bac365c2d95d0cd43
CJazzF9 <CJazzF@gmail.com>
2025-04-01 20:22:40 -0600

    Update road.js

--- pages/road.js
+++ pages/road.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 
-export default function JourneyPage() {
+export default function Road() {
   const events = [
     {
       date: 'Jan 18th',
