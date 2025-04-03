fe27eeb49d522851435ec38a309677214380e69e
CJazzF9 <CJazzF@gmail.com>
2025-04-02 19:38:44 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        headline: ['Oswald', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: []
 }
